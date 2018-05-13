dedf211957a52d15e01100674407c9e972cb6810
Hongze Zhao <zhaohongze@gmail.com>
2018-05-13 15:33:47 -0700

    Add basic usage playground

---
+++ BasicUsage.playground/Contents.swift
@@ -0,0 +1,39 @@
+//: Playground - noun: a place where people can play
+
+import Foundation
+
+let json = """
+{
+  "username": "Test user",
+  "country": {
+    "name": "United States",
+    "code": "US"
+  },
+  "type": 3
+}
+""".data(using: .utf8)!
+
+struct Country: Codable {
+  let countryName: String
+  let countryCode: String
+
+  private enum CodingKeys: String, CodingKey {
+    case countryName = "name"
+    case countryCode = "code"
+  }
+}
+
+enum UserType: Int, Codable {
+  case normal = 1
+  case admin = 2
+}
+
+struct User: Codable {
+  let username: String
+  let type: UserType
+  let country: Country
+}
+
+let decoder = JSONDecoder()
+let user = try! decoder.decode(User.self, from: json)
+
